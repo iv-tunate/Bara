2d5d3caa431c0c5283cafd4a08329899544b6535
ekestephanie1 <ekestephanie123@gmail.com>
2025-12-03 15:44:23 +0100

    added chat and notification dropdown for messages

---
+++ bara-app/app/notifications/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+
+
+
+export default function NotificationPage() {
+  
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+  Notification page coming soon 
+    </div>
+  );
+}
