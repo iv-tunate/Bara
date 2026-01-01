5da71996614cbf1142c5247be3a2059ab6c0acce
Abdurrahman Luqman Yusuf <110829892+atomic-ustadh@users.noreply.github.com>
2026-01-01 21:43:31 +0100

    Gallery Images and Grayscale
    Only the first Image works the way I want, I have even cleared the cache still not working

--- bara-app/app/page.tsx
+++ bara-app/app/page.tsx
@@ -275,11 +275,11 @@ export default function HomePage() {
               onMouseLeave={() => setHoveredImage(null)}
             >
               <Image
-                src="/sample1.png"
+                src="/Testimonial-1.png"
                 alt="Gallery Image"
                 width={150}
                 height={60}
-                className="h-[330px] w-[180px] object-cover hover:w-[350px] flex-shrink-0 transition-all duration-300 z-10 relative border-1 rounded-md"
+                className="h-[330px] w-[180px] object-cover hover:w-[350px] flex-shrink-0 transition-all duration-300 z-10 relative border-1 rounded-md grayscale hover:grayscale-0"
               />
             </div>
             <div
@@ -292,7 +292,7 @@ export default function HomePage() {
                 alt="Gallery Image"
                 width={150}
                 height={60}
-                className="flex-shrink-0 transition-transform duration-300 group-hover:scale-x-185 z-10 relative"
+                className="h-[330px] w-[180px] object-cover hover:w-[350px] flex-shrink-0 transition-all duration-300 z-10 relative border-1 rounded-md grayscale hover:grayscale-0"
               />
             </div>
             <div
@@ -305,7 +305,7 @@ export default function HomePage() {
                 alt="Gallery Image"
                 width={150}
                 height={60}
-                className="flex-shrink-0 transition-transform duration-300 group-hover:scale-x-185 z-10 relative"
+                className="h-[330px] w-[180px] object-cover hover:w-[350px] flex-shrink-0 transition-all duration-300 z-10 relative border-1 rounded-md grayscale hover:grayscale-0"
               />
             </div>
             <div
@@ -318,7 +318,7 @@ export default function HomePage() {
                 alt="Gallery Image"
                 width={150}
                 height={60}
-                className="flex-shrink-0 transition-transform duration-300 group-hover:scale-x-185 z-10 relative"
+                className="h-[330px] w-[180px] object-cover hover:w-[350px] flex-shrink-0 transition-all duration-300 z-10 relative border-1 rounded-md grayscale hover:grayscale-0"
               />
             </div>
             <div
@@ -331,7 +331,7 @@ export default function HomePage() {
                 alt="Gallery Image"
                 width={150}
                 height={60}
-                className="flex-shrink-0 transition-transform duration-300 group-hover:scale-x-185 z-10 relative"
+                className="h-[330px] w-[180px] object-cover hover:w-[350px] flex-shrink-0 transition-all duration-300 z-10 relative border-1 rounded-md grayscale hover:grayscale-0"
               />
             </div>
           </div>
